c9e3c4083d5522fe09e8c434da3a3cba244529df
halaeb <halaeb@gmail.com>
2021-12-01 11:38:57 -0600

    implement nav options

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import StreamA from './components/StreamA';
+import StreamB from './components/StreamB';
+import StreamC from './components/StreamC';
+import StreamD from './components/StreamD';
 
 function App() {
   const location = useLocation();
@@ -14,6 +17,15 @@ function App() {
           </Route>
           <Route exact path='/streamA'>
             <StreamA />
+          </Route>
+          <Route exact path='/streamB'>
+            <StreamB />
+          </Route>
+          <Route exact path='/streamC'>
+            <StreamC />
+          </Route>
+          <Route exact path='/streamD'>
+            <StreamD />
           </Route>
         </Switch>
       </AnimatePresence>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import '../tailwind.output.css';
 
 function Menu() {
+    const menuRef = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const menuClick = () => setIsOpen(!isOpen);
     return(
         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                height="40px" 
-                viewBox="0 0 24 24" 
-                width="40px" 
-                fill="#000000">
-                <path d="M0 0h24v24H0V0z" fill="none"/>
-                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-            </svg>
+            <button onClick={menuClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    height="40px" 
+                    viewBox="0 0 24 24" 
+                    width="40px" 
+                    fill="#000000">
+                    <path d="M0 0h24v24H0V0z" fill="none"/>
+                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+                    />
+                </svg>
+            </button>
+            <nav ref={menuRef} className={ isOpen ? 'visible' : 'invisible'}>
+                <ul>
+                    <li>
+                        <a href='/About'>Project Description</a>
+                    </li>
+                    <li>
+                        <a href='/Implementation'>Implementation</a>
+                    </li>
+                </ul>
+            </nav>
+
         </div>
     );
 }
