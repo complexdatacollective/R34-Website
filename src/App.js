03d93f8c8a0700ba7af0bf1e8411dead4186d329
halaeb <halaeb@gmail.com>
2021-12-01 09:54:03 -0600

    utilize useLocation hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { AnimatePresence } from 'framer-motion';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import StreamA from './components/StreamA';
 
 function App() {
+  const location = useLocation();
   return (
-    <Router>
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence exitBeforeEnter initial={false}>
         <Switch location={location} key={location.pathname}>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/streamA' component={StreamA}/>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/streamA'>
+            <StreamA />
+          </Route>
         </Switch>
       </AnimatePresence>
-    </Router>
   );
 }
 
