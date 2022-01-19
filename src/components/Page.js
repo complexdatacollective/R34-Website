848749222a15f4fb41ef390d8c0347cfdcc6684e
halaeb <halaeb@gmail.com>
2022-01-19 15:35:26 -0600

    remove horizontal animations from page component
    horizontal animation added to Stream component in stream-navigation branch and removed from this branch

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,25 +2,17 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 
-const isVertical = true;
-
 const pageVariantsVertical = {
   hidden: { y: '100vh' },
   visible: { y: 0 },
   exit: { y: '-100vh' },
 };
 
-const pageVariantsHorizontal = {
-  hidden: { x: '100vw' },
-  visible: { x: 0 },
-  exit: { x: '-100vw' },
-};
-
 const Page = function Page({ children }) {
   return (
     <motion.div
       className="container mx-auto"
-      variants={isVertical ? pageVariantsVertical : pageVariantsHorizontal}
+      variants={pageVariantsVertical}
       initial="hidden"
       animate="visible"
       exit="exit"
