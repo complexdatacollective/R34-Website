33146afe113cfd6702e7cd8f0cfbf3fd20572c2b
halaeb <halaeb@gmail.com>
2022-01-19 12:48:28 -0600

    basic animation between page components

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 
-// const pageVariants = {
-//   hidden: { y: '100vh' },
-//   visible: { y: 0 },
-//   exit: { y: '-100vh' },
-// };
+const pageVariants = {
+  hidden: { y: '100vh' },
+  visible: { y: 0 },
+  exit: { y: '-100vh' },
+};
 
 const Page = function Page({ children }) {
   return (
-    <motion.div className="container mx-auto">
+    <motion.div
+      className="container mx-auto"
+      variants={pageVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       {children}
     </motion.div>
   );
