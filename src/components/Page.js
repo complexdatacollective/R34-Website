afc201482692f306d56ae6bebe506eb040410193
halaeb <halaeb@gmail.com>
2022-01-19 15:29:30 -0600

    vertical animation between pages

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 
-const pageVariants = {
+const isVertical = true;
+
+const pageVariantsVertical = {
   hidden: { y: '100vh' },
   visible: { y: 0 },
   exit: { y: '-100vh' },
+};
+
+const pageVariantsHorizontal = {
+  hidden: { x: '100vw' },
+  visible: { x: 0 },
+  exit: { x: '-100vw' },
 };
 
 const Page = function Page({ children }) {
   return (
     <motion.div
       className="container mx-auto"
-      variants={pageVariants}
+      variants={isVertical ? pageVariantsVertical : pageVariantsHorizontal}
       initial="hidden"
       animate="visible"
       exit="exit"
